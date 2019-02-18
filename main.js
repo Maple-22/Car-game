9a8dca3666aee982553d1085cbb5ca4727c8cf99
Maple-22 <42615894+Maple-22@users.noreply.github.com>
2019-02-18 19:56:28 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -11,27 +11,61 @@ const car = {
         this.mileage += 10;
         alert(`New Mileage: ${this.mileage}`);
     },
-}
+
+    driveAroundUSA(){
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage += 7000;
+        alert(`New Mileage: ${this.mileage}`);
+        alert('Car needs a tuneup!');
+        this.isWorking = false;
+    },
+
+    getTuneUp(){
+        alert('Car is fixed and ready to go!');
+        this.isWorking = true;
+    },
+
+    honk(){
+        alert("Honk! Honk!");
+    }
+};
 
 // FUNCTIONS
-// LOGS ALL OF OUR CAR'S CURRENT STATS TO THE CONSOLE
-const reWriteStats=()->(
-    console.log('make: $(car.make)');
-    
-/*
-FUNCTION reWriteStats(){
+// Logs all of our car's current stats to the console
+reWriteStats=()=>{
+    console.log(`Make: ${car.make}`);
+    console.log(`Model: ${car.model}`);
+    console.log(`Color: ${car.color}`);
+    console.log(`Mileage: ${car.mileage}`);
+    console.log(`IsWorking: ${car.isWorking}`);
+    console.log('-----------------------------');
 
 }
-*/
+
 
 // MAIN PROCESS
-// CAPTURES KEYBOARD INPUT. DEPENDING ON THE LETTER PRESSED IT WILL "call" (execute) DIFFERENT FUNCTIONS.
+// Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
 document.onkeyup = (event) => {
-    // CAPUTUES THE KEY PRESS, CONVERTS IT TO LOWERCASE, AND SAVES IT TO A VARIABLE.
+    // Captures the key press, converts it to lowercase, and saves it to a variable
     let letter = String.fromCharCode(event.keyCode).toLowerCase();
 
     if (letter === "d") {
         car.driveToWork();
+        reWriteStats();
+    }
+
+    if (letter === "r") {
+        car.driveAroundUSA();
+        reWriteStats();
+    }
+
+    if (letter === "t") {
+        car.getTuneUp();
+        reWriteStats();
+    }
+
+    if (letter === "h") {
+        car.honk();
         reWriteStats();
     }
 
